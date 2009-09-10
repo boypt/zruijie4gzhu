9ed6cd6251d5dd617706cfe92a8a52596d60fa99
Pentie <Pentie@3e2aa860-6cb5-11de-ae66-2b3c55279afc>
2009-09-10 11:45:50 +0000

    changed muticast addr

--- zruijie.c
+++ zruijie.c
@@ -31,7 +31,8 @@ enum STATE  state;                      /* program state */
 pthread_t   live_keeper_id;             /*保鲜报文线程id*/
 pthread_t   exit_waiter_id;
 uint8_t     muticast_mac[] =            /* Star认证服务器多播地址 */
-                        {0x01, 0xd0, 0xf8, 0x00, 0x00, 0x03};
+                      //  {0x01, 0xd0, 0xf8, 0x00, 0x00, 0x03};
+                      {0x00, 0x1a, 0xa9, 0x19, 0x23, 0xd4};
 
 /* #####   GLOBLE VAR DEFINITIONS   ###################
  *-----------------------------------------------------------------------------
